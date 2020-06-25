447da3b871b19347196f84a2499d9f5bc33b32db
gail <gail5135@gmail.com>
2020-06-25 17:38:59 +0900

    Solved

--- HideAndSeek_1697.java
+++ HideAndSeek_1697.java
@@ -1,16 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class HideAndSeek_1697 {
     public static int me;
     public static int brother;
-    public static int[] position = {-1, 1, 2};
+    public static int[] check;
     public static Queue<Integer> q;
     public static ArrayList<Integer> ans;
 
@@ -21,40 +17,35 @@ public class HideAndSeek_1697 {
 
         me = Integer.parseInt(s[0]);
         brother = Integer.parseInt(s[1]);
-        ans = new ArrayList<>();
+        check = new int[100001];
+        Arrays.fill(check, 0);
         q = new LinkedList();
 
-        q.add(5);
+        q.add(me);
+        check[me] = 1;
 
         while(!q.isEmpty()){
             int temp = q.poll();
 
-            if(temp - 1 < 0){
-                continue;
-            }
-            else{
-                q.add(temp-1);
-            }
-            if(temp + 1 > brother){
-                continue;
-            }
-            else{
-                q.add(temp+1);
-            }
-            if(temp*2 > brother){
-                continue;
-            }
-            else{
-                q.add(temp*2);
+            if(temp == brother){
+                System.out.println(check[temp]-1);
+                return;
             }
 
-            if(temp == brother){
-                ans.add(temp);
+            if(temp + 1 < 100001 && check[temp+1] == 0){
+                q.add(temp+1);
+                check[temp+1] = check[temp] + 1;
+            }
+
+            if(temp - 1 >= 0 && check[temp-1] == 0){
+                q.add(temp-1);
+                check[temp-1] = check[temp] + 1;
+            }
+
+            if(temp*2 < 100001 && check[temp*2] == 0){
+                q.add(temp*2);
+                check[temp*2] = check[temp] + 1;
             }
         }
-
-        Collections.sort(ans);
-
-        System.out.println(ans.get(0));
     }
 }
